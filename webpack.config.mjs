13f385347cf31549404bf3ee273b75db6b6fe5c5
Lucas Garron <code@garron.net>
2023-03-24 17:07:03 -0700

    Webpack: preserve `import.meta.url` and `import.meta.resolve`.
    Unfortunately, this doesn't help it pass any more tests.

--- webpack.config.mjs
+++ webpack.config.mjs
@@ -1,9 +1,17 @@
 export default {
   mode: "production",
+  target: "es2022",
   entry: {
-    "entry": "./test/entry.js", "entry-verbose": "./test/entry-verbose.js"
+    "entry": "./test/entry.js",
+    "entry-verbose": "./test/entry-verbose.js"
   },
   output: {
+    chunkFormat: "module",
     path: new URL("./dist/test-webpack", import.meta.url).pathname,
   },
+  module: {
+    parser: {
+      javascript : { importMeta: false }
+    }
+  }
 };
